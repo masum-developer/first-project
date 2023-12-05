7f53c80c362b81a50d2b4125da71b5ed7af809ae
Masum Billah <fabrika786@gmail.com>
2023-12-06 01:23:52 +0600

    done student create

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    res.status(200).json({
+      success: true,
+      message: 'Data Created Successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Sudents are retrieved Successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  const { studentId } = req.params;
+  try {
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    // res.send(result)
+    res.status(200).json({
+      success: true,
+      message: 'Student get successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
